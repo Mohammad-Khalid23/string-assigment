fade20049374430dc5a304f896fdb68381389e9e
Mohammad Khalid <m.k.ayub23@gmail.com>
2017-05-02 22:28:41 +0500

    string

---
+++ app.js
@@ -0,0 +1,154 @@
+/*************Mohammad Khalid , Orange Section , String Assignment*****************/
+
+
+
+/******Task 1*****/
+
+// var firstName = prompt ( "Enter first name "); 
+// var lastName = prompt ( "Enter lasst name ");
+//  var fullName = firstName + lastName ; 
+//   console.log ("Assalam u Alykum !"+ fullName ); 
+
+
+
+/******Task 2*****/
+// var mobileModel = prompt("Enter favrourite Mobile Model ");
+
+// console.log( "My favourite Mobile is: " +mobileModel);
+
+// console.log("length of String: "+mobileModel.length);
+
+
+/******Task 3*****/
+
+// var str = "Pakistan";
+
+// console.log("String : "+str);
+// console.log("Index of 'n' :"+str.indexOf("n"));
+
+/******Task 4*****/
+// var greet = "hello world";
+// console.log("String : "+greet);
+// console.log("Index of 'l' :"+greet.lastIndexOf("l"));
+
+
+/******Task 5*****/
+
+
+// var str = "Pakistan";
+
+// console.log("String : "+str);
+// console.log("Character at 3rd Index : "+str.charAt(3));
+
+
+/******Task 6*****/
+
+// var firstName = prompt ( "Enter first name "); 
+// var lastName = prompt ( "Enter lasst name ");
+//  var fullName = firstName.concat(lastName); 
+//   console.log ("Assalam u Alykum !"+ fullName ); 
+
+//                                 /******Task 7*****/
+// var city = "hyderabad"; 
+// console.log(city); 
+// var newCity = city.replace("hyder","Islam");;
+// console.log(newCity);                               
+
+
+/******Task 8*****/
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+
+// console.log(message);
+// for ( var i = 0 ; i <= message.length ; i++){
+
+//     if ( message.slice(i , i + 3) === "and"){
+//         message = message.slice( 0 ,i) + "&" +message.slice(i + 3);
+//     }
+// }
+// console.log(message);
+
+
+/******Task 9*****/
+// var  userInput = prompt("Enter a number");
+// console.log("Value: "+userInput)
+// console.log(typeof(userInput));
+// userInput = parseInt(userInput);
+// console.log("Value: "+userInput)
+// console.log(typeof(userInput));
+
+
+
+/******Task 10*****/
+// var inputURl = prompt ("Enter a URL", "www.facebook.com");
+
+// var newURL = inputURl.slice(4);
+// console.log(newURL);
+
+/******Task 11*****/
+
+// var userInput = prompt("Enter a Word or String");
+
+// console.log("UserInput :"+userInput);
+// console.log("UpperCase"+userInput.toUpperCase());
+/******Task 12*****/
+// var userInput = prompt("Enter a Word or String");
+
+// console.log("UserInput :"+userInput);
+// console.log("LowerCase :"+userInput.toLowerCase());
+
+/******Task 13*****/
+
+// var userInput = prompt("Enter a String");
+
+// userInput = userInput.split(" ");
+// // userInput = userInput[0].slice(0, 1).toUpperCase();
+// // console.log(userInput);
+
+// for (var i = 0; i <= userInput.length; i++) {
+// userInput[i] = userInput[i].slice( 0,2);
+// }
+// console.log(userInput);
+
+/******Task 14*****/
+// var inputDecimal = prompt("Enter a decimal Number");
+// console.log("Number :"+inputDecimal);
+
+//  for( var i =0 ; i <= inputDecimal.length ; i++){
+
+//      if( inputDecimal.slice(i , i+1) === "."){
+
+//          inputDecimal = inputDecimal.slice(0,i) +inputDecimal.slice(i + 1);
+//      }
+//  } 
+// console.log("Result :"+inputDecimal);
+
+/******Task 15*****/
+
+// var firstInput = prompt("Enter value of a ");
+
+// var secondInput = prompt("Enter value of b ");
+
+// if( firstInput ==3 && secondInput == 3){
+
+//     var x = firstInput+secondInput;
+//     console.log("a is "+firstInput);
+//     console.log("b is "+secondInput);
+//    console.log("a + b "+x);
+
+// }
+
+/******Task 16*****/
+
+
+// var firstInput = parseInt( prompt("Enter value of a "));
+
+// var secondInput = parseInt(prompt("Enter value of b ") );
+
+// if( firstInput ==3 && secondInput == 3){
+
+//     var x = firstInput - secondInput;
+//     console.log("a is "+firstInput);
+//     console.log("b is "+secondInput);
+//    console.log("a - b "+x);
+
+// }
